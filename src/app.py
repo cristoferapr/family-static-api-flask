4ad2decb65e7762d21ddf3c850cc7e36c7970355
cristoferapr <102608682+cristoferapr@users.noreply.github.com>
2023-04-18 20:36:41 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,6 +14,9 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+jackson_family.add_member("John", 33, [7, 13, 22])
+jackson_family.add_member("Jane", 35, [10, 14, 3])
+jackson_family.add_member("Jimmy", 5, [1])
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,17 +29,43 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route("/member/<int:member_id>")
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is not None:
+        return jsonify(member), 200
+    return jsonify("member id not found"), 404
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    data = request.get_json()
+    
+    if 'first_name' not in data or 'age' not in data or 'lucky_numbers' not in data:
+        return jsonify({'error': 'missing required fields'}), 400
+
+    member_id = data.get('id')
+    if member_id:
+        member = jackson_family.get_member(member_id)
+        if member is not None:
+            return jsonify({'error': f'member with ID {member_id} already exists'}), 400
+    else:
+        # Generate a new member ID
+        member_id = jackson_family._generateId()
+
+    jackson_family.add_member(data['first_name'], data['age'], data['lucky_numbers'], member_id)
+    
+    return jsonify("member added succesfully"), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    result = jackson_family.delete_member(member_id)
+    if 'error' in result:
+        return jsonify(result), 400
+    return jsonify(result), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
